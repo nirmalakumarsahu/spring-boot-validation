b0a4cf9879c7875b2301173f3eba749ea82e8903
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-27 00:43:47 +0530

    model

---
+++ src/main/java/com/sahu/springboot/basics/model/Product.java
@@ -0,0 +1,25 @@
+package com.sahu.springboot.basics.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
+
+}
